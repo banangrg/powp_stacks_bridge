fdc229410834abbc59c2a9b64eeac0d82134ca9d
Grzegorz Banach <19409@edu.p.lodz.pl>
2017-03-09 11:53:23 +0100

    3.1.8 encapsulation in StackHanoi v3

--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -5,14 +5,22 @@ public class StackHanoi extends Stack {
 	private int totalRejected = 0;
 
 	public int reportRejected() {
-		return totalRejected;
+		return getTotalRejected();
 	}
 
 	public void push(int itemToPush) {
 		if (!isEmpty() && itemToPush > getTop()) {
-			totalRejected++;
+			setTotalRejected(getTotalRejected() + 1);
 		} else {
 			super.push(itemToPush);
 		}
+	}
+
+	private int getTotalRejected() {
+		return totalRejected;
+	}
+
+	private void setTotalRejected(int totalRejected) {
+		this.totalRejected = totalRejected;
 	}
 }
